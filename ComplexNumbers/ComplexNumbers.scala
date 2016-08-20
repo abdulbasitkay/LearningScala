6fdc473f2fb0eed46ea4cc0df23adfeb7bfbe7c3
AbdulBasit KABIR <abdulbasitkay@gmail.com>
2016-08-20 14:49:22 +0100

    initial commit

---
+++ ComplexNumbers/ComplexNumbers.scala
@@ -0,0 +1,9 @@
+import scala.language.postfixOps
+
+object ComplexNumbers {
+  def main(args: Array[String]) {
+    val c = new Complex(1.2, 3.4)
+    println("imaginary part: " + c.im)
+    println(c toString)
+  }
+}
